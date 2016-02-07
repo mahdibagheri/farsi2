f053af87440ac8ec33cc81190f3e45d7ec9ab47d
mahdibagheri <150mahdi@gmail.com>
2016-02-07 12:47:30 -0800

    Update tosupport.lua

--- plugins/tosupport.lua
+++ plugins/tosupport.lua
@@ -1,9 +1,8 @@
---- https://github.com/amirhmz
---- https://telegram.me/AmirDark
+
 do
 
 function run(msg, matches)
-  return " The Support Invition Link : \n https://telegram.me/joinchat/ClGL-QOZQ4QjIccSChlA8g"
+  return " The Support Invition Link : \n https://telegram.me/joinchat/Bzt_MAOXEtzRc6BrpE34jg"
   end
 return {
   description = "shows support link", 
@@ -15,5 +14,3 @@ return {
   run = run
 }
 end
---- https://github.com/amirhmz
---- https://telegram.me/AmirDark
